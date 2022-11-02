a447f114121ab57dd6b855f5d9804f0775c66cf6
Royner39 <roynerarturo39@gmail.com>
2022-11-01 19:15:19 -0600

    Test MedicoService Funcional

--- src/test/java/tec/qa/veterinaria/services/ClienteServiceTest.java
+++ src/test/java/tec/qa/veterinaria/services/ClienteServiceTest.java
@@ -1,51 +1,29 @@
 package tec.qa.veterinaria.services;
 
 import org.junit.jupiter.api.Test;
-import tec.qa.veterinaria.model.Cliente;
-
-import java.util.List;
+import tec.qa.veterinaria.controller.ClienteController;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClienteServiceTest extends ClienteService {
+class ClienteServiceTest extends ClienteController {
 
     @Test
-    void testListar() {
-        List<Cliente> clientes = listar();
-        assertNotNull(clientes);
+    void listar() {
     }
 
     @Test
-    void testListarId() {
-        int cedula = 1;
-        Cliente cliente = listarId(cedula).get();
-        assertNotNull(cliente);
+    void listarId() {
     }
 
     @Test
-    void testLogin() {
-        int cedula = 1;
-        String password = "123";
-        boolean login = login(cedula, password);
-        assertTrue(login);
+    void login() {
     }
 
     @Test
-    void testSave() {
-        Cliente cliente = new Cliente();
-        cliente.setCedula(1);
-        cliente.setNombre("Juan");
-        cliente.setTelefono(12345678);
-        cliente.setEmail("prueba@gmail.com");
-        cliente.setPassword("123");
-        boolean save = save(cliente);
-        assertTrue(save);
+    void save() {
     }
 
     @Test
-    void testDelete() {
-        int cedula = 1;
-        boolean delete = delete(cedula);
-        assertTrue(delete);
+    void delete() {
     }
 }
--- src/test/java/tec/qa/veterinaria/services/MedicoServiceTest.java
+++ src/test/java/tec/qa/veterinaria/services/MedicoServiceTest.java
@@ -1,33 +1,94 @@
 package tec.qa.veterinaria.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tec.qa.veterinaria.model.Medico;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+
+@SpringBootTest
 class MedicoServiceTest extends MedicoService {
+
+    @Autowired
+    private MedicoService medicoService;
+
+    private  Medico medicoPrueba = new Medico();
+
+    @BeforeEach
+    void setUp() {
+
+        medicoPrueba.setNombre("TEST");
+        medicoPrueba.setEmail("test@vet.com");
+        medicoPrueba.setPassword("test123");
+        medicoService.save(medicoPrueba);
+
+    }
+    @AfterEach
+    void tearDown(){
+        medicoService.delete(medicoPrueba.getId());
+    }
 
     @Test
     void testListar() {
-        fail("Not yet implemented");
+        List<Medico> medicos = medicoService.listar();
+        Assertions.assertNotNull(medicos);
     }
 
     @Test
     void testListarId() {
-        fail("Not yet implemented");
+        int id = 0;
+        List<Medico> medicos = medicoService.listar();
+        for (Medico medico:medicos) {
+            if (medicoPrueba.getNombre().equals(medico.getNombre())){
+                id = medico.getId();
+            }
+        }
+        Medico medico = medicoService.listarId(id).get();
+        Assertions.assertNotNull(medico);
     }
 
     @Test
     void testLogin() {
-        fail("Not yet implemented");
+        int id = 0;
+        List<Medico> medicos = medicoService.listar();
+        for (Medico medico:medicos) {
+            if (medicoPrueba.getNombre().equals(medico.getNombre())){
+                id = medico.getId();
+            }
+        }
+        String password = "test123";
+        boolean loginSucces = medicoService.login(id,password);
+        Assertions.assertTrue(loginSucces);
     }
 
     @Test
     void testSave() {
-        fail("Not yet implemented");
+        Medico medico1 = new Medico();
+        medico1.setNombre("SAVETEST");
+        medico1.setEmail("test@vet.com");
+        medico1.setPassword("test123");
+        boolean saveSucces = medicoService.save(medico1);
+        if (saveSucces) {
+            medicoService.delete(medico1.getId());
+        }
+        Assertions.assertTrue(saveSucces);
     }
 
     @Test
     void testDelete() {
-        fail("Not yet implemented");
+        int id = 0;
+        List<Medico> medicos = medicoService.listar();
+        for (Medico medico:medicos) {
+            if (medicoPrueba.getNombre().equals(medico.getNombre())){
+                id = medico.getId();
+            }
+        }
+        boolean deleteSucces = medicoService.delete(id);
+        Assertions.assertTrue(deleteSucces);
     }
 }
